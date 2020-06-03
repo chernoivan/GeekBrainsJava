456801a3adeb2a227f2ebbb1ccc1330a3c865f9d
Alex Chernoivan <alexchernoivan@gmail.com>
2020-06-03 18:15:29 +0300

    Home-work-5

--- src/com/company/samurai/lesson5/Multithreading.java
+++ src/com/company/samurai/lesson5/Multithreading.java
@@ -1,79 +1,50 @@
 package com.company.samurai.lesson5;
 
-public class Multithreading {
-    private static final int size = 10000000;
-    private static final int half = size / 2;
-    private static float[] arr = new float[size];
+import java.util.Arrays;
 
+public class Multithreading {
+    private static final int SIZE = 10000000;
+    private static final int HALF = SIZE / 2;
+    private static Float[] oneThreadArr = new Float[SIZE];
+    private static Float[] multiThreadArr = new Float[SIZE];
 
     public static void main(String[] args) {
-        for (int i = 0; i < size; i++) {
-            arr[i] = 1;
-        }
+        Arrays.fill(oneThreadArr, 1.0f);
+        Arrays.fill(multiThreadArr, 1.0f);
 
-        calculation(arr);
-        multithreadingCalculation(arr);
-    }
+        long startTime = System.currentTimeMillis();
+        calculate(oneThreadArr, 0, SIZE);
+        System.out.println("Выполнение для одного потока: " + (System.currentTimeMillis() - startTime) + "мс");
 
-    public static void calculation(float arr[]) {
-        long a = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        Float[] arr1 = new Float[HALF];
+        Float[] arr2 = new Float[HALF];
 
-        }
-        System.out.println("lead time for one thread = " + (System.currentTimeMillis() - a));
-    }
+        startTime = System.currentTimeMillis();
+        System.arraycopy(multiThreadArr, 0, arr1, 0, HALF);
 
-    public static void multithreadingCalculation(float arr[]) {
-        float[] arr1 = new float[half];
-        float[] arr2 = new float[half];
-        long a = System.currentTimeMillis();
-        System.arraycopy(arr, 0, arr1, 0, half);
-        System.arraycopy(arr, half, arr2, 0, half);
+        Thread thread1 = new Thread(() -> calculate(arr1, 0, HALF));
+        Thread thread2 = new Thread(() -> calculate(multiThreadArr, HALF, SIZE));
 
-        MyThread t1 = new MyThread(arr1);
-        MyThread t2 = new MyThread(arr2);
-
-        t1.start();
-        t2.start();
+        thread1.start();
+        thread2.start();
 
         try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
 
-        arr1 = t1.getArr();
-        arr2 = t2.getArr();
-
-        System.arraycopy(arr1, 0, arr, 0, half);
-        System.arraycopy(arr2, 0, arr, half, half);
-        System.out.println("lead time for multithreading = " + (System.currentTimeMillis() - a));
-    }
-}
-
-class MyThread extends Thread {
-    private float[] arr;
-
-    MyThread(float[] arr) {
-        this.arr = arr;
+        System.arraycopy(multiThreadArr, HALF, arr2, 0, HALF);
+        System.arraycopy(arr1, 0, multiThreadArr, 0, HALF);
+        System.arraycopy(arr2, 0, multiThreadArr, HALF, HALF);
+        System.out.println("Выполнение для двух потоков: " + (System.currentTimeMillis() - startTime) + "мс");
     }
 
-    float[] getArr() {
-        return arr;
-    }
 
-    @Override
-    public void run() {
-        calculate();
-    }
-
-    private void calculate() {
-        int len = arr.length;
-
-        for (int i = 0; i < len; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    public static synchronized void calculate(Float arr[], int start, int stop) {
+        for (int i = start; i < stop; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
     }
 }
